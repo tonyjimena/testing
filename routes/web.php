011670af12a4fe27d0509657048df924ca261823
Tony Jimena <jimenaalvarez.tony@gmail.com>
2024-06-13 15:41:27 +0200

    init

---
+++ routes/web.php
@@ -0,0 +1,17 @@
+<?php
+
+use App\Models\User;
+use Illuminate\Support\Facades\Route;
+
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/users', function () {
+
+    $users = User::all();
+
+    return response()->json($users);
+});
+
